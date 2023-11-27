f22ff4e2d58e54f6bb08a2189d7f31facbe816e1
Katya <polikarpova_kate@mail.ru>
2023-11-27 19:35:11 +0300

    4.8. Поиск максимального элемента в коллекции

--- src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -2,28 +2,30 @@ package ru.stqa.pft.addressbook.model;
 
 import java.util.Objects;
 
-
-
 public final class GroupData {
-    private final String id;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private int id;
     public final String name;
     public final String header;
 
-    public String getId() {
+    public final String footer;
+
+    public int getId() {
         return id;
     }
 
-    public final String footer;
 
-    public GroupData(String name, String header, String footer) {
-        this.id = null;
+    public GroupData( String name, String header, String footer) {
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
-    public GroupData(String id,String name, String header, String footer) {
-        this.id = id;
+    public GroupData(int id,String name, String header, String footer) {
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -54,7 +56,7 @@ public final class GroupData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return id.equals(groupData.id) && name.equals(groupData.name);
+        return id == groupData.id && Objects.equals(name, groupData.name);
     }
 
     @Override
